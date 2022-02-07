73a9f9f6001bbfd2fdd3d335eda77b1490cc82de
zetkajot <czechuziom@gmail.com>
2022-02-07 23:33:43 +0100

    Small fix to FindMany action definition

--- src/use-cases/storage-adapter/entity-actions.ts
+++ src/use-cases/storage-adapter/entity-actions.ts
@@ -1,4 +1,4 @@
-export type FindMany<Entity, EntityData> = (params: EntityData) => Promise<Entity[]>;
+export type FindMany<Entity, EntityData> = (params: Partial<EntityData>) => Promise<Entity[]>;
 export type FindOne<Entity, UniqueParamType> = (uniqueParam: UniqueParamType) => Promise<Entity>;
 export type DeleteOne<Entity> = (entity: Entity) => Promise<void>;
 export type SaveOne<Entity> = (entity: Entity) => Promise<void>;
