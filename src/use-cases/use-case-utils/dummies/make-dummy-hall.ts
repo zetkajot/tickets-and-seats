873d302761438fc853f953d4e064da410a099526
zetkajot <czechuziom@gmail.com>
2022-02-08 16:17:09 +0100

    Extended functionality of dummy hall factory

--- src/use-cases/use-case-utils/dummies/make-dummy-hall.ts
+++ src/use-cases/use-case-utils/dummies/make-dummy-hall.ts
@@ -5,20 +5,30 @@ interface DummmyHallFactoryInput {
   id?: string,
   name?: string,
   layout?: SeatLayout,
+  requiredSeats?: number[],
 }
 
 export default function makeDummyHall(
   input: DummmyHallFactoryInput = defaultDummyHallFactoryInput,
 ): Hall {
-  const { id, name, layout } = {
+  const {
+    id, name, layout, requiredSeats,
+  } = {
     ...defaultDummyHallFactoryInput,
     ...input,
   };
-  return new Hall(id as string, name as string, layout as SeatLayout);
+  const hall = new Hall(id as string, name as string, layout as SeatLayout);
+  if ((requiredSeats as number[]).length > 0) {
+    (requiredSeats as number[]).forEach(
+      (seatNo) => (hall.hasSeat(seatNo) ? null : hall.layout.addSeat(seatNo, [0, 0])),
+    );
+  }
+  return hall;
 }
 
 const defaultDummyHallFactoryInput: DummmyHallFactoryInput = {
   id: 'example hall id',
   name: 'example hall',
   layout: new SeatLayout(),
+  requiredSeats: [],
 };
