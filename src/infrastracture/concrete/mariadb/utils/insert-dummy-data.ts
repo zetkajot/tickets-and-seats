1f4104c8f4093a95e44498ddebe21be0996e5616
zetkajot <czechuziom@gmail.com>
2022-02-14 16:31:24 +0100

    Expanded dummy test data set

--- src/infrastracture/concrete/mariadb/utils/insert-dummy-data.ts
+++ src/infrastracture/concrete/mariadb/utils/insert-dummy-data.ts
@@ -7,6 +7,7 @@ export default async function insertDummyData(pool: Pool): Promise<void> {
       ['hall-id-1', 'hall no 1', '[]'],
       ['hall-id-2', 'hall no 2', '[[1, 0, 0]]'],
       ['hall-id-3', 'hall no 3', '[[2, 0, 0], [1, 10, 10]]'],
+      ['hall-id-4', 'hall no 4', '[]'],
     ],
   );
   await pool.batch(
@@ -16,6 +17,7 @@ export default async function insertDummyData(pool: Pool): Promise<void> {
       ['event-id-2', 'event no 2', 'hall-id-3', new Date('2013').getTime(), new Date('2019').getTime(), true, '[2, 1]'],
       ['event-id-3', 'event no 3', 'hall-id-2', new Date('2032').getTime(), new Date('2034').getTime(), true, '[]'],
       ['event-id-4', 'event no 4', 'hall-id-2', new Date('2032').getTime(), new Date('2034').getTime(), false, '[]'],
+      ['event-id-5', 'event no 5', 'hall-id-2', new Date('2032').getTime(), new Date('2034').getTime(), true, '[]'],
     ],
   );
   await pool.batch(
