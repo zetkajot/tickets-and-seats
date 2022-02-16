8a6f57730f01efc5cc2ffd14f2b154817a4c4552
zetkajot <czechuziom@gmail.com>
2022-02-16 23:05:16 +0100

    Implemented SELECT query builder

---
+++ src/infrastracture/concrete/mariadb/query-creator/concrete-builders/select-query-builder.ts
@@ -0,0 +1,32 @@
+import { BuiltQuery } from '../types/built-query';
+import QueryBuilder from '../types/query-builder';
+
+export default class SelectQueryBuilder implements QueryBuilder {
+  private parameterValues: any[] = [];
+
+  setTableName(name: string): void {
+    this.parameterValues[0] = name;
+  }
+
+  setField(name: string, value: any): void {
+    this.parameterValues.push(name, value);
+  }
+
+  buildQuery(): BuiltQuery {
+    return {
+      query: this.buildQueryString(),
+      values: this.parameterValues,
+    };
+  }
+
+  private buildQueryString(): string {
+    const fieldParams = this.parameterValues.slice(1);
+    const whereClauseParams: string[] = [];
+    fieldParams.reduce((shouldPush) => {
+      if (shouldPush) whereClauseParams.push('?=?');
+      return !shouldPush;
+    }, false);
+    const afterWhereClause = whereClauseParams.join(' AND ');
+    return `SELECT * FROM ?${afterWhereClause.length > 0 ? ` WHERE ${afterWhereClause}` : ''};`;
+  }
+}
