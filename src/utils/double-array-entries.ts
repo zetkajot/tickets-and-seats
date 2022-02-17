7b25598c6c2cd4a89924b926396d5bcb4b1f2843
zetkajot <czechuziom@gmail.com>
2022-02-17 14:45:11 +0100

    Removed unused code

--- src/utils/double-array-entries.ts
+++
@@ -1,8 +0,0 @@
-export default function doubleArrayEntries(array: any[]): any[] {
-  const resultingArray = Array(array.length * 2);
-  array.forEach((val, idx) => {
-    resultingArray[idx * 2] = val;
-    resultingArray[idx * 2 + 1] = val;
-  });
-  return resultingArray;
-}
