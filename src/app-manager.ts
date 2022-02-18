e31c22fc1066459ed3f72540fb075eb369967c47
zetkajot <czechuziom@gmail.com>
2022-02-18 22:34:03 +0100

    Added debug logs

--- src/app-manager.ts
+++ src/app-manager.ts
@@ -31,7 +31,7 @@ export default class AppManager {
     const routeSchema = parseRouteSchema(path.join(__dirname, `..${path.sep}`, 'schemas', 'route_schema.json'));
     const controller = new Controller(storageVendor, controllerSchema);
     this.gateway = new ExpressGateway(routeSchema, controller);
-    const port = + <string>process.env.PORT || expressConfig.port;
+    const port = +<string>process.env.PORT || expressConfig.port;
     console.log(`App binding to ${port}`);
     await this.gateway.open(port);
   }
