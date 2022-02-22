ad941dddd816a4b0847e9b01d4e8f6c26427fefe
zetkajot <czechuziom@gmail.com>
2022-02-22 04:28:33 +0100

    Simplified Route Handler

--- src/gateways/express/express-route-handler.ts
+++ src/gateways/express/express-route-handler.ts
@@ -3,19 +3,17 @@ import e from 'express';
 export default class ExpressRouteHandler {
   constructor(private app: e.Application) {}
 
-  public addGET(path: string, handler: e.RequestHandler) {
-    this.app.get(path, handler);
-  }
-
-  public addPOST(path: string, handler: e.RequestHandler) {
-    this.app.post(path, handler);
-  }
-
-  public addPUT(path: string, handler: e.RequestHandler) {
-    this.app.put(path, handler);
-  }
-
-  public addDELETE(path: string, handler: e.RequestHandler) {
-    this.app.delete(path, handler);
+  public addRoute(
+    method: 'GET' | 'POST' | 'DELETE' | 'PUT',
+    path: string,
+    handler: e.RequestHandler,
+  ) {
+    const routeAdder = {
+      GET: this.app.get.bind(this, path, handler as any),
+      POST: this.app.post.bind(this, path, handler as any),
+      PUT: this.app.put.bind(this, path, handler as any),
+      DELETE: this.app.delete.bind(this, path, handler as any),
+    }[method] ?? (() => { throw new Error('Unknown method!'); });
+    routeAdder();
   }
 }
